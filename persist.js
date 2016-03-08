92b06e3b82c19a8c164ed08661a76c5402b6ddbb
Achmad Mahardi <achmad@mahardi.me>
2016-03-08 11:55:22 +0700

    fix redis

--- persist.js
+++ persist.js
@@ -8,7 +8,8 @@ var redis = require('redis')
   , operationComplete = false
   , ts = Math.floor(Date.now() / 1000);
 
-client = redis.createClient(process.env.OPENSHIFT_GEAR_DIR + 'tmp/redis.sock');
+client = redis.createClient();
+client.auth(process.env.REDIS_PASSWORD)
 
 /*
  * TBD: there should be error handling on all of these to handle no reply responses.
